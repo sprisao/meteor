b455076fedd0c3ec4ab550827fc2e9c99d513b5a
르네상스맨 <whdtmek@gmail.com>
2022-08-05 08:38:32 +0900

    Firebase 연동 및 이메일 회원가입 테스트

--- app/src/main/java/com/example/tinder_test/auth/JoinActivity.kt
+++ app/src/main/java/com/example/tinder_test/auth/JoinActivity.kt
@@ -9,12 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.tinder_test.R
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.android.synthetic.main.activity_intro.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_join.*
 
 class JoinActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
@@ -23,8 +27,22 @@ class JoinActivity : AppCompatActivity() {
             val email = findViewById<TextInputEditText>(R.id.emailArea)
             val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
 
-            Log.d(TAG, email.text.toString())
-            Log.d(TAG, pwd.text.toString())
+//            Log.d(TAG, email.text.toString())
+//            Log.d(TAG, pwd.text.toString())
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    }
+                }
         }
+
     }
+
+
 }
