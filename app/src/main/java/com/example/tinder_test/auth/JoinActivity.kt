3fc9e91c810b66b0747d58a29b78d306415f1c3f
BruceChoe <whdtmek@gmail.com>
2022-08-08 16:56:20 +0900

    회원 가입시 추가 정보 받아와서 Realtime DataBase에 저장 + 이때 DataModel 토대로 데이터 정리되어 저장되도록 설정

--- app/src/main/java/com/example/tinder_test/auth/JoinActivity.kt
+++ app/src/main/java/com/example/tinder_test/auth/JoinActivity.kt
@@ -1,12 +1,12 @@
 package com.example.tinder_test.auth
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tinder_test.MainActivity
 import com.example.tinder_test.R
+import com.example.tinder_test.utils.FirebaseRef
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -14,32 +14,51 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_join.*
 
 class JoinActivity : AppCompatActivity() {
+
+    private val TAG = "JoinActivity"
     private lateinit var auth: FirebaseAuth
+
+    private var uid = ""
+    private var gender = ""
+    private var city = ""
+    private var age = ""
+    private var nickname = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
-        btnJoin.setOnClickListener{
+//         닉네임, 성별, 지역, 나이, UID
+
+        btnJoin.setOnClickListener {
             val email = findViewById<TextInputEditText>(R.id.emailArea)
             val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
+
+            gender = findViewById<TextInputEditText>(R.id.genderArea).text.toString()
+            city = findViewById<TextInputEditText>(R.id.cityArea).text.toString()
+            age = findViewById<TextInputEditText>(R.id.ageArea).text.toString()
+            nickname = findViewById<TextInputEditText>(R.id.nickNameArea).text.toString()
 
             auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user  = auth.currentUser
-                        Log.d(TAG, user?.uid.toString())
+
+                        val user = auth.currentUser
+                        uid = user?.uid.toString()
+
+                        val userModel = UserDataModel(
+                            uid, nickname, age, gender, city
+                        )
+
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
+
+                        FirebaseRef.userInfoRef.child(uid).setValue(userModel)
+
                     } else {
-                        // If sign in fails, display a message to the user.
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     }
                 }
         }
-
     }
-
 }
---
+++ app/src/main/java/com/example/tinder_test/utils/FirebaseRef.kt
@@ -0,0 +1,12 @@
+package com.example.tinder_test.utils
+
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class FirebaseRef {
+
+    companion object {
+        val database = Firebase.database("https://tinderclone-736d2-default-rtdb.asia-southeast1.firebasedatabase.app")
+        val userInfoRef = database.getReference("userInfo")
+    }
+}
