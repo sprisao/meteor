8de335d16461ec343f89e266f336568c8dcaa61a
BruceChoe <whdtmek@gmail.com>
2022-08-17 09:24:04 +0900

    FirebaseService.kt에 CreateChannel & Sendnotification 메서드 추가

--- app/src/main/java/com/example/tinder_test/message/fcm/FirebaseService.kt
+++ app/src/main/java/com/example/tinder_test/message/fcm/FirebaseService.kt
@@ -1,6 +1,13 @@
 package com.example.tinder_test.message.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.tinder_test.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -24,5 +31,43 @@ class FirebaseService : FirebaseMessagingService(){
         Log.e(TAG, message.notification?.title.toString())
         Log.e(TAG, message.notification?.body.toString())
 
+        var title = message.notification?.title.toString()
+        var body = message.notification?.body.toString()
+
+        createNotificationChannel()
+        sendNotification(title, body)
     }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "name"
+            val descriptionText = "description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("Test_Channel", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(title: String, body: String){
+        var builder = NotificationCompat.Builder(this, "Test_Channel")
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                .bigText("Don't make your match to wait too long."))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)){
+            notify(123, builder.build())
+        }
+    }
+
+
 }
